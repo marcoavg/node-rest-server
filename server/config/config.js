809e640e0d91a60ffc9f80821e2acf70435a2c49
marcoavg <marcoavg1@gmail.com>
2018-07-13 10:51:00 -0500

    varible de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb://MarcoAVG:MarcoAVG01@ds135441.mlab.com:35441/cafe-marco-avg'
+    urlDB = process.env.MONGO_URI
 }
 process.env.URLDB = urlDB
 
